fefc27da76c4fa3692b9f23c3a7358b64475fb26
Ivan Rados <ivan.rados12354@gmail.com>
2023-05-21 21:31:39 +0200

    small change

--- availableShifts/AvailableShifts.js
+++ availableShifts/AvailableShifts.js
@@ -3,16 +3,17 @@ import { sortByCity } from "../maintenance/allAvailableShifts";
 import { useEffect, useState } from "react";
 
 const AvailableShifts = () => {
-    const [shiftsByCity, setShiftsByCity] = useState([])
-    useEffect(() => {
-        sortByCity(setShiftsByCity)
-    }, [])
-    console.log(shiftsByCity)    
-  return (
+  const [shiftsByCity, setShiftsByCity] = useState([]);
+  useEffect(() => {
+    sortByCity(setShiftsByCity);
+  }, []);
+  return shiftsByCity.length > 0 ? (
     <View>
       <Text>AvailableShifts</Text>
     </View>
+  ) : (
+    <Text>No shifts for you!</Text>
   );
 };
 
-export default AvailableShifts
+export default AvailableShifts;
